1e50e74b0ebcc2e2b7facb7ff80d34d0f6dd7845
Kyllox <kyllox4804@gmail.com>
2024-03-18 10:48:16 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import random
 
+range_input = input("랜덤 범위를 콤마(,)로 구분지어 입력해주세요 ([n1, n2]): ")
+num_range = range_input.split(',')
+
 first = []
-for i in range(100):
-    first[i] = random.randint(0, 1)
+for i in range(100000):
+    first.append(random.randint(int(num_range[0]), int(num_range[1])))
 
 second = []
-for i in range(100):
-    second[i] = random.randint(0, 99)
+for i in range(100000):
+    second.append(random.randint(0, 99999))
 
 ran = []
-counts = []
+counts = [0] * (int(num_range[1]) + 1)
 
 repeat_count = int(input("시뮬레이션 횟수를 지정해주세요: "))
+
 for i in range(repeat_count):
-    ran[i] = first[random.choice(second)]
+    ran.append(first[random.choice(second)])
     counts[ran[i] - 1] += 1
 
-    print(i + "차:", ran[i])
+    print(str(i + 1) + "차:", ran[i])
 
 print("=====[ 시뮬레이션 결과 ]=====")
-for i in range(len(counts)):
-    print(i + ":", counts[i] + "회, 확률", (counts[i] / repeat_count) * 100 + "%")
+for i in range(int(num_range[0]), (int(num_range[1]) + 1)):
+    print(str(i) + ":", str(counts[i - 1]) + "회, 확률", str((counts[i - 1] / repeat_count) * 100) + "%")
